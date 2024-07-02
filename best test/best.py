802365da34c71dd7dc77f3b121edc38c605ce9e1
galash <galash@bitbucket.org>
2024-07-02 15:13:56 +0300

    optimiz; print top 10

--- best test/best.py
+++ best test/best.py
@@ -1,14 +1,29 @@
 from ultralytics import YOLO
 import glob
 
+top10_predict = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+top10_image = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'] 
+
 model = YOLO('best.pt')
 
 images = glob.glob('*.jpg')
 for image in images:
     result = model.predict(image)
     box = result[0].boxes
+    predict = box.conf[0]
     
     if len(box.conf) < 1:
         continue
     
-    print(f'{image} : {box.conf[0]}')
+    for i in range(0, 10):
+        if predict >= top10_predict[i]:
+            top10_predict[i] = predict
+            top10_image[i] = image
+            
+            print(f'({i})  {image} : {predict}')
+            
+            break
+ 
+print("\n@@ result: @@\n")
+for i in range(0, 10):
+    print(f'({i})  {image} : {predict}')
