671200d9f840277cb0182440f46ee5556b7001db
Galash <galash@bitbucket.org>
2024-07-10 20:14:36 +0300

    improve top 10

--- best test/best.py
+++ best test/best.py
@@ -1,13 +1,20 @@
 from ultralytics import YOLO
 import glob
 
-top10_predict = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
-top10_image = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'] 
+top10_predict_0 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+top10_image_0 = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'] 
+
+top10_predict_1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0] 
+top10_image_1 = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0'] 
 
 model = YOLO('best.pt')
 
 images = glob.glob('*.jpg')
 for image in images:
+    conf = image.split(".")[0]
+    f = open(f'{conf}.txt', "r")
+    typ = f.read()[0]
+    
     result = model.predict(image)
     box = result[0].boxes
     
@@ -17,14 +24,35 @@ for image in images:
     predict = box.conf[0]
     
     for i in range(0, 10):
-        if predict >= top10_predict[i]:
-            top10_predict[i] = predict
-            top10_image[i] = image
+        if typ == 0 and predict >= top10_predict_0[i]:
+            for j in range(i + 1, 10):
+                top10_predict_0[j] = top10_predict_0[j - 1]
+                top10_image_0[j] = top10_image_0[j - 1]
+                
+            top10_predict_0[i] = predict
+            top10_image_0[i] = image
+            
+            print(f'({i})  {image} : {predict}')
+            
+            break
+        
+        if typ == 1 and predict >= top10_predict_1[i]:
+            for j in range(i + 1, 10):
+                top10_predict_1[j] = top10_predict_1[j - 1]
+                top10_image_1[j] = top10_image_1[j - 1]
+                
+            top10_predict_1[i] = predict
+            top10_image_1[i] = image
             
             print(f'({i})  {image} : {predict}')
             
             break
  
 print("\n@@ result: @@\n")
+print("\n 0 - \n")
 for i in range(0, 10):
-    print(f'({i})  {top10_image[i]} : {top10_predict[i]}')
+    print(f'({i})  {top10_image_0[i]} : {top10_predict_0[i]}')
+    
+print("\n 1 - \n")
+for i in range(0, 10):
+    print(f'({i})  {top10_image_1[i]} : {top10_predict_1[i]}')
